026757669222850b0175a2844e1a9dd18a5f7d4a
Your Nameclear <you@example.com>
2022-07-18 10:08:09 +0200

    create new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.css'
+import AboutUs from './components/AboutYs'
+import DailyPrices from './components/DailyPrices'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Newsletter from './components/Newletter'
+import Roadmap from './components/Roadmao'
+import ScrollToTop from './components/ScrollToTop'
+import Services from './components/Services'
+import SignUp from './components/SignUp'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <ScrollToTop/>
+        <Navbar/>
+        <Home/>
+        <DailyPrices/> 
+        <AboutUs/>
+        <Services/>
+        <SignUp/>
+        <Roadmap/>
+        <Newsletter/>
+        <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
